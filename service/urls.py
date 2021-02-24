b577e94b3d9a5cc5b36e28b625085605a34e9bca
thuan20132000 <thuan20132000@gmail.com>
2021-02-24 09:10:59 +0700

    Add static file

--- service/urls.py
+++ service/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('api/v1/category',views.category_list_api,name='category_list_api'),
     path('api/v1/fields',views.field_list_api,name='field_list_api'),
     path('api/v1/jobs',views.job_list_api,name='job_list_api'),
-
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- service247/settings.py
+++ service247/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,7 +94,7 @@ DATABASES = {
         'USER':'truongthuan',
         'PASSWORD':'Thuan123',
         'HOST':'127.0.0.1',
-        'PORT':'5432'
+        'PORT':'5432',
     }
 }
 
@@ -135,3 +136,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
