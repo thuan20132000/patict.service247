cad93d3b61b456466890591613018b782c88a20f
thuan20132000 <thuan20132000@gmail.com>
2021-04-15 09:02:55 +0700

    Update api

--- service247/settings.py
+++ service247/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'service247.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'service247_master',
+        'NAME': 'service247_master_v2',
         'USER':'thuantruong',
         'PASSWORD':'Thuan123',
         'HOST':'18.141.229.83',
